03927bc1147d4a23e2f2c86001e1185ea87ab0c2
Sabrina <sabrina.costa06@outlook.com>
2021-03-03 15:46:45 -0300

    difinir fim de jogo e estilizar

--- script.js
+++ script.js
@@ -46,6 +46,14 @@ function iniciarJogo(){
     if(snake[0].y> 15 * box && direction == 'down') snake[0].y=0;
     if(snake[0].y< 0 * box && direction == 'up') snake[0].y=16*box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo); /* para a função jogo quando a cobra colide com o proprio corpo*/
+            alert('Game Over');
+        }
+
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
